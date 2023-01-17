30a83c8dc2946e59802748b904123cf67ad7bae2
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2023-01-17 14:40:45 +0100

    casi terminado

--- proyecto_en_equipo.py
+++ proyecto_en_equipo.py
@@ -4,39 +4,40 @@ def main(page: ft.Page):
     page.title = "Proyecto Futbolístico"
     #page.vertical_alignment = ft.MainAxisAlignment.CENTER//// esto es para centrarlo en el medio
 
+    def cambiarFoto(e):
+        if (equipos.value == "Cadiz CF"):
+            img.src = "Imágenes/CadizCF.jpg"
+                
+        elif (equipos.value == "AOVE Villacarrillo"):
+            img.src = "Imágenes/AOVE Villacarrillo.png"
+                
+        elif (equipos.value == "Málaga CF"):
+            img.src = "Imágenes/MalagaCF.png"
+                
+            
+        elif (equipos.value == "NewCastle UFC"):
+            img.src = "Imágenes/NewcastleUFC.pmg"
+                
+        page.update()
 
-    equipos = ft.Dropdown(label="Equipos",width=400,options=[ft.dropdown.Option("Cadiz CF"),
+
+
+    equipos = ft.Dropdown(label="Equipos",on_change=cambiarFoto, width=400,options=[ft.dropdown.Option("Cadiz CF"),
                                                             ft.dropdown.Option("AOVE Villacarrillo"),
                                                             ft.dropdown.Option("Club Deportivo Alcoyano"),
                                                             ft.dropdown.Option("Málaga CF"),
-                                                            ft.dropdown.Option("NewCastle UFC"),] ,on_change=dropdown_changed
-    )
-    page.add(equipos)
+                                                            ft.dropdown.Option("NewCastle UFC"),])
 
 
+    img = ft.Image(src=f"g")
 
-    #equipos_imagen = ""
 
-    def dropdown_changed(e):
-        if (equipos.value == "Cadiz CF"):
-            equipos_imagen = "Imágenes/CadizCF.jpg"
-            
-        
-        elif (equipos.value == "AOVE Villacarrillo"):
-            equipos_imagen = "Imágenes/AOVE Villacarrillo.png"
-            print("platano al poder")
-
-        elif (equipos.value == "Málaga CF"):
-            equipos_imagen = "Imágenes/MalagaCF.png"
-            print("platano al poder")
-        
-        elif (equipos.value == "NewCastle UFC"):
-            equipos_imagen = "Imágenes/NewcastleUFC.pmg"
-            
-        equipos_imagen.src=f"{equipos_imagen}"
-        page.update()
-        equipos_imagen = ft.Image(src=f"CadizCF.jpg")
+    page.add(equipos,img)
     
-        page.add(equipos_imagen)
 
+
+    
+
+       
+   
 ft.app(target=main,assets_dir="Imágenes")

5159287834e3a6c9d39768995bf0341437928cd2
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2023-01-17 14:28:42 +0100

    no se que narices le pasa al on change

--- proyecto_en_equipo.py
+++ proyecto_en_equipo.py
@@ -3,36 +3,40 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Proyecto Futbolístico"
     #page.vertical_alignment = ft.MainAxisAlignment.CENTER//// esto es para centrarlo en el medio
-    
-    def boton(e):
-        t.value = f"Equipo seleccionado:  {equipos.value}"
-        page.update()
 
-    t = ft.Text()
-    b = ft.ElevatedButton(text="seleccionar", on_click=boton)
+
     equipos = ft.Dropdown(label="Equipos",width=400,options=[ft.dropdown.Option("Cadiz CF"),
-                                                            ft.dropdown.Option("Villacarrillo"),
+                                                            ft.dropdown.Option("AOVE Villacarrillo"),
                                                             ft.dropdown.Option("Club Deportivo Alcoyano"),
                                                             ft.dropdown.Option("Málaga CF"),
-                                                            ft.dropdown.Option("New Castle UFC"),])
-    page.add(equipos, b, t)
+                                                            ft.dropdown.Option("NewCastle UFC"),] ,on_change=dropdown_changed
+    )
+    page.add(equipos)
 
 
 
+    #equipos_imagen = ""
 
-    img = ft.Image(src=f"CadizCF.jpg")
+    def dropdown_changed(e):
+        if (equipos.value == "Cadiz CF"):
+            equipos_imagen = "Imágenes/CadizCF.jpg"
+            
+        
+        elif (equipos.value == "AOVE Villacarrillo"):
+            equipos_imagen = "Imágenes/AOVE Villacarrillo.png"
+            print("platano al poder")
+
+        elif (equipos.value == "Málaga CF"):
+            equipos_imagen = "Imágenes/MalagaCF.png"
+            print("platano al poder")
+        
+        elif (equipos.value == "NewCastle UFC"):
+            equipos_imagen = "Imágenes/NewcastleUFC.pmg"
+            
+        equipos_imagen.src=f"{equipos_imagen}"
+        page.update()
+        equipos_imagen = ft.Image(src=f"CadizCF.jpg")
     
-    page.add(img)
-
-
-
-
-
-
-
-
-
-
-
+        page.add(equipos_imagen)
 
 ft.app(target=main,assets_dir="Imágenes")
